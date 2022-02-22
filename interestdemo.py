1164698b09a9bd5499220d4c53a610970ad81f15
Pavithran <thranpavi916@gmail.com>
2022-02-22 20:19:39 +0530

    Create interestdemo.py

---
+++ interestdemo.py
@@ -0,0 +1,6 @@
+#simple interest
+P = int(input("enter the value of p"))
+R = int(input("enter the value of r"))
+T = int(input("enter the value of t"))
+si = (P * R * T) / 100
+print("The interest is" ,si)
